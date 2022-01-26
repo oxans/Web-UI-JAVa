c188a49f674735b975a0815b0565e78aa775933b
malti <oxanas517@gmail.com>
2022-01-26 22:39:45 +0300

    test

---
+++ src/main/java/org/example/Lesson03/FantlabTestLike.java
@@ -0,0 +1,14 @@
+package org.example.Lesson03;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FantlabTestLike {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://fantlab.ru/");
+
+
+    }
+}
